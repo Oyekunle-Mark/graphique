227d3498c184ad336c5156a7c9519179d2df8cda
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-09 12:31:54 +0100

    Earliest ancestor challenge

--- src/earliest_ancestor/ans.py
+++ src/earliest_ancestor/ans.py
@@ -14,19 +14,21 @@ class Queue:
     def size(self):
         return len(self.storage)
 
+
 class Graph:
     """ 
         Represent a graph as a dictionary of verts 
         mapping labels to edges 
-    
+
     """
+
     def __init__(self):
         self.vertices = {}
-    
+
     def add_vertex(self, vertex_id):
         if vertex_id not in self.vertices:
             self.vertices[vertex_id] = set()
-    
+
     def add_edge(self, v1, v2):
         if v1 in self.vertices and v2 in self.vertices:
             self.vertices[v1].add(v2)
